fc1975ffdd40d9369d197ec6dc99960c973e7022
ssplist <sspatil.nbnscoe@gmail.com>
2025-06-24 14:05:26 +0530

    Create EmployeeDao.java

---
+++ employee/src/main/java/com/example/employee/dao/EmployeeDao.java
@@ -0,0 +1,12 @@
+package com.example.employee.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.employee.dto.Employee;
+import com.example.employee.repository.EmployeeRepository;
+
+public class EmployeeDao {
+
+}
